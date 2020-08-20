7b3d805d5cf2b5257a7d10986fcd048449784a8d
Joey Gallotta <gallo@DESKTOP-T9R25LI>
2020-08-20 13:05:41 -0700

    Add project files.

---
+++ ChessBoardGUIApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoardGUIApp
+{
+    public partial class Form1 : Form
+    {
+        // reference to class board
+        static Board myBoard = new Board(8);
+        // 2d array of buttons 
+        public Button[,] btnGrid = new Button[myBoard.Size, myBoard.Size];
+        public Form1()
+        {
+            InitializeComponent();
+         // similar to printBoard
+            populateGrid();
+        }
+
+        private void populateGrid()
+        {
+            int buttonSize = panel1.Width / myBoard.Size;
+            panel1.Height = panel1.Width;
+
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    btnGrid[i, j] = new Button();
+
+                    btnGrid[i, j].Height = buttonSize;
+                    btnGrid[i, j].Width = buttonSize;
+
+                    btnGrid[i, j].Click += Grid_Button_Click;
+
+                    panel1.Controls.Add(btnGrid[i, j]);
+                    btnGrid[i, j].Location = new Point(i * buttonSize, j * buttonSize);
+
+                    btnGrid[i, j].Text = i + "|" + j;
+                }
+            }
+
+        }
+
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChessBoardModel/Board.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+     public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] theGrid { get; set; }
+
+        //constructor
+        public Board(int s)
+        {
+            //initial size of board is s
+            Size = s;
+            //initialize new 2 dimensional grid
+            theGrid = new Cell[Size, Size];
+
+            //initiate values in grid 
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+
+        }
+        public void MarkNextLegalMove(Cell currentCell, string chessPiece)
+        {
+            //clear board 
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+            switch (chessPiece)
+            {
+                case "Knight":
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    break;
+
+                case "King":
+                    break;
+
+                case "Rook":
+                    break;
+
+                case "Bishop":
+                    break;
+
+                case "Queen":
+                    break;
+
+                default:
+                    break;
+
+
+            }
+            theGrid[currentCell.RowNumber, currentCell.ColumnNumber].CurrentlyOccupied = true;
+        }
+    }
+}
+
